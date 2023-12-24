252edb583c4ce6a8b66978832b581088ef8811b1
diegogirao <diegoramiro25@gmail.com>
2023-12-24 14:47:12 +0000

    feat added middlewaer for handling with cors policy

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,21 @@ import express from "express"
 import mongoose from "mongoose"
 import { PORT, mongodbURL } from "./config.js"
 import booksRoute from "./routes/booksRoute.js"
+import cors from "cors"
 
 const app = express()
 
 // Middleware for parsing request body
 app.use(express.json())
+
+// Middleware for handling CORS POLICY - Allow Custom Origins
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type"],
+	})
+)
 
 app.use("/books", booksRoute)
 
