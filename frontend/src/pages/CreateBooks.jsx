d0835eeabf47834a09c46f611650724e39346bc0
diegogirao <diegoramiro25@gmail.com>
2023-12-26 19:19:00 +0000

    feat created create books component

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,10 +1,78 @@
+import { useState } from "react"
+import Skeleton from "../components/Skeleton"
 import BackButton from "../components/BackButton"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
-function CreateBooks() {
+const CreateBooks = () => {
+	const [title, setTitle] = useState("")
+	const [author, setAuthor] = useState("")
+	const [publishYear, setPublishYear] = useState("")
+	const [loading, setLoading] = useState(false)
+	const navigate = useNavigate()
+	const handleSaveBook = () => {
+		const data = {
+			title,
+			author,
+			publishYear,
+		}
+		setLoading(true)
+		axios
+			.post("http://localhost:5555/books", data)
+			.then(() => {
+				setLoading(false)
+				navigate("/")
+			})
+			.catch((error) => {
+				setLoading(false)
+				alert("An error happened. Please Check the console")
+				console.log(error)
+			})
+	}
+
 	return (
-		<div>
+		<div className="p-4">
 			<BackButton />
-			CreateBooks
+			<h1 className="text-3xl my-4">Create Book</h1>
+			{loading ? <Skeleton /> : ""}
+			<div className="flex flex-col border-2 border-sky-400 rounded-xl w-[37.5rem] p-4 mx-auto">
+				<div className="my-4">
+					<label className="text-xl mr-4 text-gray-500">Title</label>
+					<input
+						type="text"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						className="rounded-xl border-2 border-gray-500 px-4 py-2 w-full"
+						required
+					/>
+				</div>
+				<div className="my-4">
+					<label className="text-xl mr-4 text-gray-500">Author</label>
+					<input
+						type="text"
+						value={author}
+						onChange={(e) => setAuthor(e.target.value)}
+						className="rounded-xl border-2 border-gray-500 px-4 py-2 w-full"
+						required
+					/>
+				</div>
+				<div className="my-4">
+					<label className="text-xl mr-4 text-gray-500">Publish Year</label>
+					<input
+						type="number"
+						value={publishYear}
+						onChange={(e) => setPublishYear(e.target.value)}
+						className="rounded-xl border-2 border-gray-500 px-4 py-2 w-full"
+						required
+					/>
+				</div>
+				<button
+					className="rounded-xl p-2 text-lg font-semibold text-white bg-sky-500 m-8 hover:bg-sky-800"
+					onClick={handleSaveBook}
+				>
+					Save
+				</button>
+			</div>
 		</div>
 	)
 }
