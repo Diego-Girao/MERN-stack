8048efe284ce966ee969b5f04a89253ddb4f8a2d
diegogirao <diegoramiro25@gmail.com>
2023-12-26 18:43:32 +0000

    fix changed return response from one book

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -48,11 +48,8 @@ router.get("/", async (request, response) => {
 router.get("/:id", async (request, response) => {
 	try {
 		const { id } = request.params
-		const books = await Book.findById(id)
-		return response.status(200).json({
-			count: books.length,
-			data: books,
-		})
+		const book = await Book.findById(id)
+		return response.status(200).json(book)
 	} catch (error) {
 		console.log(error.message)
 		response.status(500).send({ message: error.message })
