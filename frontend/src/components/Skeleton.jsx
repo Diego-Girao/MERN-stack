98399f682d141246a6042bd4abceff154238d71b
diegogirao <diegoramiro25@gmail.com>
2023-12-26 16:54:01 +0000

    fix changed component name spinner to skeleton

---
+++ frontend/src/components/Skeleton.jsx
@@ -0,0 +1,107 @@
+function Skeleton() {
+	return (
+		<div className="rounded-md p-2 max-w-7xl w-full mx-auto">
+			<div className="animate-pulse flex space-x-4">
+				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
+				<div className="flex-1 space-y-6 py-1">
+					<div className="grid grid-cols-4 gap-4">
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+					</div>
+				</div>
+			</div>
+			<div className="animate-pulse flex space-x-4">
+				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
+				<div className="flex-1 space-y-6 py-1">
+					<div className="grid grid-cols-4 gap-4">
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+					</div>
+				</div>
+			</div>
+			<div className="animate-pulse flex space-x-4">
+				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
+				<div className="flex-1 space-y-6 py-1">
+					<div className="grid grid-cols-4 gap-4">
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+					</div>
+				</div>
+			</div>
+			<div className="animate-pulse flex space-x-4">
+				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
+				<div className="flex-1 space-y-6 py-1">
+					<div className="grid grid-cols-4 gap-4">
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+					</div>
+				</div>
+			</div>
+			<div className="animate-pulse flex space-x-4">
+				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
+				<div className="flex-1 space-y-6 py-1">
+					<div className="grid grid-cols-4 gap-4">
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+					</div>
+				</div>
+			</div>
+			<div className="animate-pulse flex space-x-4">
+				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
+				<div className="flex-1 space-y-6 py-1">
+					<div className="grid grid-cols-4 gap-4">
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+					</div>
+				</div>
+			</div>
+			<div className="animate-pulse flex space-x-4">
+				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
+				<div className="flex-1 space-y-6 py-1">
+					<div className="grid grid-cols-4 gap-4">
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+					</div>
+				</div>
+			</div>
+			<div className="animate-pulse flex space-x-4">
+				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
+				<div className="flex-1 space-y-6 py-1">
+					<div className="grid grid-cols-4 gap-4">
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+					</div>
+				</div>
+			</div>
+			<div className="animate-pulse flex space-x-4">
+				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
+				<div className="flex-1 space-y-6 py-1">
+					<div className="grid grid-cols-4 gap-4">
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Skeleton
