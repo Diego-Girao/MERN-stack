92f254cbbcf6f2ecb8843d2249668a6979ac4e25
diegogirao <diegoramiro25@gmail.com>
2023-12-26 00:01:30 +0000

    style changed icon shape and color

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,15 +1,12 @@
 import { Link } from "react-router-dom"
-import { BsArrowLeft } from "react-icons/bs"
+import { TbSquareRoundedArrowLeftFilled } from "react-icons/tb"
 
 // eslint-disable-next-line react/prop-types
 const BackButton = ({ destination = "/" }) => {
 	return (
 		<div className="flex">
-			<Link
-				to={destination}
-				className="bg-sky-800 text-white px-4 py-1 rounded-lg w-fit"
-			>
-				<BsArrowLeft className="text-2xl" />
+			<Link to={destination} className="mx-2 my-2 rounded-lg w-fit">
+				<TbSquareRoundedArrowLeftFilled className="size-8 text-sky-500 hover:text-black hover:duration-200 hover:ease-linear" />
 			</Link>
 		</div>
 	)
