6ddea849e2d27b08e06bc0f5be181f4812dfa924
diegogirao <diegoramiro25@gmail.com>
2023-12-27 22:05:47 +0000

    style new skeleton animation and icon

--- frontend/src/components/Skeleton.jsx
+++ frontend/src/components/Skeleton.jsx
@@ -1,103 +1,26 @@
+import { GiBookshelf } from "react-icons/gi"
+
 function Skeleton() {
 	return (
-		<div className="rounded-md p-2 max-w-7xl w-full mx-auto">
-			<div className="animate-pulse flex space-x-4">
-				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="grid grid-cols-4 gap-4">
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-					</div>
+		<div className="absolute inset-0 flex justify-center items-center">
+			<div className="w-full h-fit flex justify-center gap-8 items-center select-none opacity-55">
+				<div
+					className="flex text-neutral-500 animate-pulse"
+					style={{ animationDelay: "0s" }}
+				>
+					<GiBookshelf className="size-14" />
 				</div>
-			</div>
-			<div className="animate-pulse flex space-x-4">
-				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="grid grid-cols-4 gap-4">
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-					</div>
+				<div
+					className="flex text-neutral-600 animate-pulse"
+					style={{ animationDelay: "0.4s" }}
+				>
+					<GiBookshelf className="size-14" />
 				</div>
-			</div>
-			<div className="animate-pulse flex space-x-4">
-				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="grid grid-cols-4 gap-4">
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-					</div>
-				</div>
-			</div>
-			<div className="animate-pulse flex space-x-4">
-				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="grid grid-cols-4 gap-4">
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-					</div>
-				</div>
-			</div>
-			<div className="animate-pulse flex space-x-4">
-				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="grid grid-cols-4 gap-4">
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-					</div>
-				</div>
-			</div>
-			<div className="animate-pulse flex space-x-4">
-				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="grid grid-cols-4 gap-4">
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-					</div>
-				</div>
-			</div>
-			<div className="animate-pulse flex space-x-4">
-				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="grid grid-cols-4 gap-4">
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-					</div>
-				</div>
-			</div>
-			<div className="animate-pulse flex space-x-4">
-				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="grid grid-cols-4 gap-4">
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-					</div>
-				</div>
-			</div>
-			<div className="animate-pulse flex space-x-4">
-				<div className="rounded-md bg-gradient-to-br from-gray-200 via-gray-400 to-slate-600 opacity-40 h-5 w-7"></div>
-				<div className="flex-1 space-y-6 py-1">
-					<div className="grid grid-cols-4 gap-4">
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-						<div className="h-5 bg-gradient-to-br from-gray-200 via-gray-400 to-gray-600 opacity-40"></div>
-					</div>
+				<div
+					className="flex text-neutral-700 animate-pulse"
+					style={{ animationDelay: "0.6s" }}
+				>
+					<GiBookshelf className="size-14" />
 				</div>
 			</div>
 		</div>
