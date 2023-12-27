193bee21607a37efa7d87fc5e3bb7267d0c76057
diegogirao <diegoramiro25@gmail.com>
2023-12-27 22:06:21 +0000

    feat added set timeout function

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,18 +9,21 @@ import { TbEdit } from "react-icons/tb"
 const Home = () => {
 	const [books, setBooks] = useState([])
 	const [loading, setLoading] = useState(false)
+
 	useEffect(() => {
 		setLoading(true)
-		axios
-			.get("http://localhost:5555/books")
-			.then((response) => {
-				setBooks(response.data.data)
-				setLoading(false)
-			})
-			.catch((error) => {
-				console.log(error)
-				setLoading(false)
-			})
+		setTimeout(() => {
+			axios
+				.get("http://localhost:5555/books")
+				.then((response) => {
+					setBooks(response.data.data)
+					setLoading(false)
+				})
+				.catch((error) => {
+					console.log(error)
+					setLoading(false)
+				})
+		}, 3000)
 	}, [])
 
 	return (
