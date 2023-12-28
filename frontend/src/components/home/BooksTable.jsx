82c2fe3481cc5240114535346bcf357d3ca75065
diegogirao <diegoramiro25@gmail.com>
2023-12-28 23:52:24 +0000

    feat creating book table component

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -1,5 +1,58 @@
-const BooksTable = () => {
-	return <div>BooksTable</div>
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom"
+import { FcInfo } from "react-icons/fc"
+import { VscTrash } from "react-icons/vsc"
+import { TbEdit } from "react-icons/tb"
+
+const BooksTable = ({ books }) => {
+	return (
+		<table className="w-full border-separate border-spacing-2">
+			<thead>
+				<tr>
+					<th className="border border-slate-600 rounded-md">No</th>
+					<th className="border border-slate-600 rounded-md">Title</th>
+					<th className="border border-slate-600 rounded-md max-md:hidden">
+						Author
+					</th>
+					<th className="border border-slate-600 rounded-md max-md:hidden">
+						Publish Year
+					</th>
+					<th className="border border-slate-600 rounded-md">Operations</th>
+				</tr>
+			</thead>
+			<tbody>
+				{books.map((book, index) => (
+					<tr key={book._id} className="h-8">
+						<td className="border border-slate-700 rounded-md text-center">
+							{index + 1}
+						</td>
+						<td className="border border-slate-700 rounded-md text-center">
+							{book.title}
+						</td>
+						<td className="border border-slate-700 rounded-md text-center max-md:hidden">
+							{book.author}
+						</td>
+						<td className="border border-slate-700 rounded-md text-center max-md:hidden">
+							{book.publishYear}
+						</td>
+						<td className="border border-slate-700 rounded-md text-center">
+							<div className="flex justify-center gap-x-4">
+								<Link to={`/books/details/${book._id}`}>
+									<FcInfo className="text-2xl text-emerald-600" />
+								</Link>
+								<Link to={`/books/edit/${book._id}`}>
+									<TbEdit className="text-2xl text-yellow-400" />
+								</Link>
+								<Link to={`/books/delete/${book._id}`}>
+									<VscTrash className="text-2xl text-red-600" />
+								</Link>
+							</div>
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	)
 }
 
 export default BooksTable
