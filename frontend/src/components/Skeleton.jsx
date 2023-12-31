aa2ce5dbceddf7b418216b658c59659ca818db26
diegogirao <diegoramiro25@gmail.com>
2023-12-31 17:13:54 +0000

    style: change colors and size

--- frontend/src/components/Skeleton.jsx
+++ frontend/src/components/Skeleton.jsx
@@ -5,22 +5,22 @@ function Skeleton() {
 		<div className="absolute inset-0 flex justify-center items-center">
 			<div className="w-full h-fit flex justify-center gap-8 items-center select-none opacity-55">
 				<div
-					className="flex text-neutral-500 animate-pulse"
+					className="flex text-sky-400 animate-pulse"
 					style={{ animationDelay: "0s" }}
 				>
-					<GiBookshelf className="size-14" />
+					<GiBookshelf className="size-10" />
 				</div>
 				<div
-					className="flex text-neutral-600 animate-pulse"
+					className="flex text-sky-700 animate-pulse"
 					style={{ animationDelay: "0.4s" }}
 				>
-					<GiBookshelf className="size-14" />
+					<GiBookshelf className="size-10" />
 				</div>
 				<div
-					className="flex text-neutral-700 animate-pulse"
+					className="flex text-sky-900 animate-pulse"
 					style={{ animationDelay: "0.6s" }}
 				>
-					<GiBookshelf className="size-14" />
+					<GiBookshelf className="size-10" />
 				</div>
 			</div>
 		</div>
