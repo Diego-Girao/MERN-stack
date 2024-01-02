205f18e195d03236405693490c193bfee0386fc2
diegogirao <diegoramiro25@gmail.com>
2024-01-02 20:06:54 +0000

    style: change table head background color

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -9,15 +9,21 @@ const BooksTable = ({ books }) => {
 		<table className="w-full border-separate border-spacing-2">
 			<thead>
 				<tr>
-					<th className="border border-slate-600 rounded-md">No</th>
-					<th className="border border-slate-600 rounded-md">Title</th>
-					<th className="border border-slate-600 rounded-md max-md:hidden">
+					<th className="border border-slate-600 bg-black text-white rounded-md">
+						No
+					</th>
+					<th className="border border-slate-600 bg-black text-white rounded-md">
+						Title
+					</th>
+					<th className="border border-slate-600 bg-black text-white rounded-md max-md:hidden">
 						Author
 					</th>
-					<th className="border border-slate-600 rounded-md max-md:hidden">
+					<th className="border border-slate-600 bg-black text-white rounded-md max-md:hidden">
 						Publish Year
 					</th>
-					<th className="border border-slate-600 rounded-md">Operations</th>
+					<th className="border border-slate-600 bg-black text-white rounded-md">
+						Operations
+					</th>
 				</tr>
 			</thead>
 			<tbody>
